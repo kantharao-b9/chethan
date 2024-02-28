b37e31507f373acd24402c17229dabc5f2ac5553
kantharao-b9 <kantharao.b@gmail.com>
2024-02-28 15:53:36 +0200

    add 2 methods

--- demo/src/main/java/com/example/demo/Kantharao.java
+++ demo/src/main/java/com/example/demo/Kantharao.java
@@ -9,5 +9,10 @@ public class DemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
-Sysout();
+public static void main1(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+public static void main2(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
 }
